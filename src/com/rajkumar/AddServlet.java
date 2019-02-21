f9a51cf35fc5373ade2e9aef246effb1f06c48cc
rajkumar <rajkumarnavhaal@gmail.com>
2019-02-21 22:08:34 +0530

    servlet

---
+++ src/com/rajkumar/AddServlet.java
@@ -0,0 +1,28 @@
+package com.rajkumar;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AddServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		int i=Integer.parseInt(req.getParameter("num1"));
+		int j=Integer.parseInt(req.getParameter("num2"));
+		
+		int k=i+j;
+		res.getWriter().println("result is "+k);
+		
+		RequestDispatcher rd=req.getRequestDispatcher("sq");
+//		rd.forward(req, res);
+		
+		
+	}	
+	
+}
